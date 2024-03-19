af2ba36d3bbaa20ffedc6d75edbdc4d5e3d02529
Ismoil Zaripov <ismoilzaripov55@gmail.com>
2024-03-19 18:45:42 +0500

    Microservices project updated #2

--- user-service/src/main/java/uz/app/userservice/application/controllers/UserRestController.java
+++ user-service/src/main/java/uz/app/userservice/application/controllers/UserRestController.java
@@ -1,24 +1,29 @@
 package uz.app.userservice.application.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uz.app.userservice.domain.models.request.UserRequest;
 import uz.app.userservice.domain.models.response.UserResponse;
+import uz.app.userservice.domain.usecases.UserUseCase;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserRestController {
+
+    private final UserUseCase userUseCase;
 
     @PostMapping
     public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest userRequest) {
-        return null;
+        return ResponseEntity.ok(userUseCase.createUser(userRequest));
     }
 
     @GetMapping("/{userId}")
     public ResponseEntity<UserResponse> getUser(@PathVariable Integer userId) {
-        return null;
+        return ResponseEntity.ok(userUseCase.getUser(userId));
     }
 
     @PutMapping("/{userId}")
@@ -26,11 +31,11 @@ public class UserRestController {
             @PathVariable Integer userId,
             @RequestBody UserRequest userRequest
     ) {
-        return null;
+        return ResponseEntity.ok(userUseCase.updateUser(userId, userRequest));
     }
 
     @GetMapping
     public ResponseEntity<List<UserResponse>> usersList() {
-        return null;
+        return ResponseEntity.ok(userUseCase.usersList());
     }
 }
